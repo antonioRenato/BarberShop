a963928178ebbbd0f7133f9abb3ff1b986d9fd78
antonioRenato <antoniorpfs@gmail.com>
2025-03-24 07:59:31 -0300

    Created validator with fluentValidation

--- src/BarberShop.Application/UseCases/Invoices/Register/RegisterInvoiceUseCase.cs
+++ src/BarberShop.Application/UseCases/Invoices/Register/RegisterInvoiceUseCase.cs
@@ -13,25 +13,16 @@ namespace BarberShop.Application.UseCases.Invoices.Register
 
         private void Validate(RequestInvoiceJson request)
         {
-            if (string.IsNullOrWhiteSpace(request.Description))
-                throw new ArgumentException("The Description is required!");
+            var validator = new RegisterInvoiceValidator();
 
-            if (request.Amount <= 0)
-                throw new ArgumentException("The amount must be greater than zero");
+            var result = validator.Validate(request);
 
-            var resultOfCompareDate = DateTime.Compare(request.Date, DateTime.UtcNow);
+            if (!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(x => x.ErrorMessage).ToList();
 
-            if (resultOfCompareDate > 0)
-                throw new ArgumentException("The date is in the future.");
 
-            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-            var JobTypeIsValid = Enum.IsDefined(typeof(JobType), request.JobType);
-
-            if (!paymentTypeIsValid)
-                throw new ArgumentException("Payment Type does not exist in the current request");
-
-            if (!JobTypeIsValid)
-                throw new ArgumentException("Job Type does not exist in the current request");
+            }
         }
     }
 }
